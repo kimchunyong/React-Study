c991c47600b5d75a7f3eaea1ddfad36db5f62db0
Kimchunyong <lovemy5854@naver.com>
2019-01-21 17:47:54 +0900

    react data update

--- contact-app/src/App.js
+++ contact-app/src/App.js
@@ -27,11 +27,28 @@ class App extends Component {
     });
   }
 
+  handleUpdate = (id, data) => {
+    const { information } = this.state;
+    this.setState({
+      information: information.map(
+        info => {
+          if (info.id === id) {
+            return {
+              id,
+              ...data,
+            };
+          }
+          return info;
+        }
+      )
+    })
+  }
+
   render() {
     return (
       <div>
         <PhoneForm onCreate={this.handleCreate} />
-        <PhoneInfoList data={this.state.information} onRemove={this.handleRemove} />
+        <PhoneInfoList data={this.state.information} onRemove={this.handleRemove} onUpdate={this.handleUpdate} />
       </div>
     );
   }

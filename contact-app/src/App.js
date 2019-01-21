cc4290a4f8a79c3ba9ac9656fc6c407042161cbe
Kimchunyong <lovemy5854@naver.com>
2019-01-21 16:07:08 +0900

    react 연습

--- contact-app/src/App.js
+++ contact-app/src/App.js
@@ -1,25 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PhoneForm from './components/PhoneForm';
 
 class App extends Component {
+
+  id = 0;
+
+  state = {
+    information: [],
+  }
+  handleCreate = (data) => {
+    const { information } = this.state;
+    this.setState({
+      information: information.concat({
+        ...data,
+        id: this.id++
+      }),
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <PhoneForm onCreate={this.handleCreate} />
+        {JSON.stringify(this.state.information)}
       </div>
     );
   }
---
+++ contact-app/src/components/PhoneForm.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+
+class PhoneForm extends Component {
+    state = {
+        name: '',
+        phone: '',
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.props.onCreate({
+            name: this.state.name,
+            phone: this.state.phone
+        })
+        this.setState({
+            name: '',
+            phone: '',
+        })
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <input
+                    name="name"
+                    placeholder="이름"
+                    onChange={this.handleChange}
+                    value={this.state.name}
+                />
+                <input
+                    name="phone"
+                    placeholder="전화번호"
+                    onChange={this.handleChange}
+                    value={this.state.phone}
+                />
+                <button type="submit">등록</button>
+            </form>
+        );
+    }
+}
+
+export default PhoneForm;

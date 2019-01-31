f8d17953586b2614b8750e7d331f9a13c3169651
Kimchunyong <lovemy5854@naver.com>
2019-01-31 15:34:52 +0900

    start react - JSX

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,6 +1,12 @@
+/*
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
 const root = document.getElementById("root");
 ReactDOM.render(<App />, root);
+*/
+
+const App = function(){
+    return <div>Hi!</div>;
+}

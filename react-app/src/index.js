861a2e5b6675d32f870b0e0c217fb3061c3cdebf
Kimchunyong <lovemy5854@naver.com>
2019-02-08 15:47:51 +0900

    API KEY

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
-const API_KEY = 'AIzaSyDM7_ueFLjxqHM5y3Jyw0rxE-RcD9BH4E0';
+const API_KEY = '';
 
 const container = document.querySelector('.container');
 

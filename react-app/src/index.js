c198b7b6f1f8d8b01f27afb6d8df118c11c48e38
kimchunyong <lovemy5854@naver.com>
2019-02-14 00:28:03 +0900

    video play cmpl

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -4,6 +4,7 @@ import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 import API_KEY from './components/apiKey';
 
 class App extends Component {
@@ -21,6 +22,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoDetail video={this.state.videos[0]} />
         <VideoList videos={this.state.videos} />
       </div>
     );

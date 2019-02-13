40af9bf7fef1e83998b2572a08dbc1450d106d62
Kimchunyong <lovemy5854@naver.com>
2019-02-13 18:49:54 +0900

    .

--- react-app/src/components/video_list.js
+++ react-app/src/components/video_list.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
-import VideoListItem from './video_list_item';
+import React, { Component } from "react";
+import VideoListItem from "./video_list_item";
 
 class VideoList extends Component {
-  render () {
+  render() {
     const videoList = this.props.videos;
     const videoItems = videoList.map(video => {
       return <VideoListItem key={video.etag} video={video} />;
     });
-    return <ul className='col-md-4 list-group'>{videoItems}</ul>;
+    return <ul className="col-md-4 list-group">{videoItems}</ul>;
   }
 }
 

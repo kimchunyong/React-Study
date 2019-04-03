3ee7e103c87f9107847dde805e5cff60028d7d77
Kimchunyong <lovemy5854@naver.com>
2019-04-03 18:17:38 +0900

    [update] setState()

--- movies-app/src/App.js
+++ movies-app/src/App.js
@@ -2,26 +2,6 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie';
 
-const movies = [
-  {
-    title: 'Matrix',
-    poster: 'http://cfile230.uf.daum.net/image/120165194C241D693330B4',
-  },
-  {
-    title: 'Pull Metal Jacket',
-    poster: 'http://images.amazon.com/images/P/B00005ATQF.01.LZZZZZZZ.jpg',
-  },
-  {
-    title: 'OldBoy',
-    poster:
-      'http://img.cgv.co.kr/Movie/Thumbnail/Poster/000079/79861/79861_1000.jpg',
-  },
-  {
-    title: 'Star Wars',
-    poster:
-      'https://upload.wikimedia.org/wikipedia/ko/thumb/6/6a/%EC%8A%A4%ED%83%80%EC%9B%8C%EC%A6%88_%EA%B9%A8%EC%96%B4%EB%82%9C_%ED%8F%AC%EC%8A%A4.jpg/250px-%EC%8A%A4%ED%83%80%EC%9B%8C%EC%A6%88_%EA%B9%A8%EC%96%B4%EB%82%9C_%ED%8F%AC%EC%8A%A4.jpg',
-  },
-];
 
 class App extends Component {
   //render : componentWillMount() => render() => componentDidMount()
@@ -36,13 +16,39 @@ class App extends Component {
     // console.log(`i'm DidMount!`);
     setTimeout(() => {
       this.setState({
-        greeting: 'hellow again',
+        movies: [
+          ...this.state.movies,
+          {
+            title: "transformer",
+            poster: 'https://upload.wikimedia.org/wikipedia/ko/thumb/e/e4/%ED%8A%B8%EB%9E%9C%EC%8A%A4%ED%8F%AC%EB%A8%B8_%EC%98%81%ED%99%94.jpg/250px-%ED%8A%B8%EB%9E%9C%EC%8A%A4%ED%8F%AC%EB%A8%B8_%EC%98%81%ED%99%94.jpg',
+          }
+        ]
       });
-    }, 5000);
+    }, 3000);
   }
 
   state = {
-    greeting: 'Hello',
+
+    movies: [
+      {
+        title: 'Matrix',
+        poster: 'http://cfile230.uf.daum.net/image/120165194C241D693330B4',
+      },
+      {
+        title: 'Pull Metal Jacket',
+        poster: 'http://images.amazon.com/images/P/B00005ATQF.01.LZZZZZZZ.jpg',
+      },
+      {
+        title: 'OldBoy',
+        poster:
+          'http://img.cgv.co.kr/Movie/Thumbnail/Poster/000079/79861/79861_1000.jpg',
+      },
+      {
+        title: 'Star Wars',
+        poster:
+          'https://upload.wikimedia.org/wikipedia/ko/thumb/6/6a/%EC%8A%A4%ED%83%80%EC%9B%8C%EC%A6%88_%EA%B9%A8%EC%96%B4%EB%82%9C_%ED%8F%AC%EC%8A%A4.jpg/250px-%EC%8A%A4%ED%83%80%EC%9B%8C%EC%A6%88_%EA%B9%A8%EC%96%B4%EB%82%9C_%ED%8F%AC%EC%8A%A4.jpg',
+      },
+    ],
   };
 
   render() {
@@ -50,8 +56,7 @@ class App extends Component {
     // console.log(`i'm render`);
     return (
       <div className="App">
-        {this.state.greeting}
-        {movies.map((movie, index) => {
+        {this.state.movies.map((movie, index) => {
           return (
             <Movie title={movie.title} poster={movie.poster} key={index} />
           );

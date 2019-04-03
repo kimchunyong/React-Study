75019c682cebc9a0485c9c1183109a44733e7645
kimchunyong <lovemy58511@gmail.com>
2019-04-03 23:49:39 +0900

    [edit] loading 구현

--- movies-app/src/App.js
+++ movies-app/src/App.js
@@ -2,8 +2,9 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie';
 
-
 class App extends Component {
+  state = {};
+
   //render : componentWillMount() => render() => componentDidMount()
   //update : componentWillReciveProps() => shouldComponentUpdate => componentWillUpdate() => render() => ComponentDidUpdate()
   componentWillMount() {
@@ -17,38 +18,39 @@ class App extends Component {
     setTimeout(() => {
       this.setState({
         movies: [
-          ...this.state.movies,
           {
-            title: "transformer",
-            poster: 'https://upload.wikimedia.org/wikipedia/ko/thumb/e/e4/%ED%8A%B8%EB%9E%9C%EC%8A%A4%ED%8F%AC%EB%A8%B8_%EC%98%81%ED%99%94.jpg/250px-%ED%8A%B8%EB%9E%9C%EC%8A%A4%ED%8F%AC%EB%A8%B8_%EC%98%81%ED%99%94.jpg',
-          }
-        ]
+            title: 'Matrix',
+            poster: 'http://cfile230.uf.daum.net/image/120165194C241D693330B4',
+          },
+          {
+            title: 'Pull Metal Jacket',
+            poster:
+              'http://images.amazon.com/images/P/B00005ATQF.01.LZZZZZZZ.jpg',
+          },
+          {
+            title: 'OldBoy',
+            poster:
+              'http://img.cgv.co.kr/Movie/Thumbnail/Poster/000079/79861/79861_1000.jpg',
+          },
+          {
+            title: 'Star Wars',
+            poster:
+              'https://upload.wikimedia.org/wikipedia/ko/thumb/6/6a/%EC%8A%A4%ED%83%80%EC%9B%8C%EC%A6%88_%EA%B9%A8%EC%96%B4%EB%82%9C_%ED%8F%AC%EC%8A%A4.jpg/250px-%EC%8A%A4%ED%83%80%EC%9B%8C%EC%A6%88_%EA%B9%A8%EC%96%B4%EB%82%9C_%ED%8F%AC%EC%8A%A4.jpg',
+          },
+          {
+            title: 'transformer',
+            poster:
+              'https://upload.wikimedia.org/wikipedia/ko/thumb/e/e4/%ED%8A%B8%EB%9E%9C%EC%8A%A4%ED%8F%AC%EB%A8%B8_%EC%98%81%ED%99%94.jpg/250px-%ED%8A%B8%EB%9E%9C%EC%8A%A4%ED%8F%AC%EB%A8%B8_%EC%98%81%ED%99%94.jpg',
+          },
+        ],
       });
-    }, 3000);
+    }, 2000);
   }
 
-  state = {
-
-    movies: [
-      {
-        title: 'Matrix',
-        poster: 'http://cfile230.uf.daum.net/image/120165194C241D693330B4',
-      },
-      {
-        title: 'Pull Metal Jacket',
-        poster: 'http://images.amazon.com/images/P/B00005ATQF.01.LZZZZZZZ.jpg',
-      },
-      {
-        title: 'OldBoy',
-        poster:
-          'http://img.cgv.co.kr/Movie/Thumbnail/Poster/000079/79861/79861_1000.jpg',
-      },
-      {
-        title: 'Star Wars',
-        poster:
-          'https://upload.wikimedia.org/wikipedia/ko/thumb/6/6a/%EC%8A%A4%ED%83%80%EC%9B%8C%EC%A6%88_%EA%B9%A8%EC%96%B4%EB%82%9C_%ED%8F%AC%EC%8A%A4.jpg/250px-%EC%8A%A4%ED%83%80%EC%9B%8C%EC%A6%88_%EA%B9%A8%EC%96%B4%EB%82%9C_%ED%8F%AC%EC%8A%A4.jpg',
-      },
-    ],
+  _renderMovies = () => {
+    return this.state.movies.map((movie, idx) => {
+      return <Movie title={movie.title} poster={movie.poster} key={idx} />;
+    });
   };
 
   render() {
@@ -56,11 +58,7 @@ class App extends Component {
     // console.log(`i'm render`);
     return (
       <div className="App">
-        {this.state.movies.map((movie, index) => {
-          return (
-            <Movie title={movie.title} poster={movie.poster} key={index} />
-          );
-        })}
+        {this.state.movies ? this._renderMovies() : 'Loading'}
       </div>
     );
   }

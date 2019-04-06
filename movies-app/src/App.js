26c2619e000c92c9c9e9edd66e3d2f320a0661e5
kimchunyong <lovemy58511@gmail.com>
2019-04-06 11:11:42 +0900

    [update] async , await

--- movies-app/src/App.js
+++ movies-app/src/App.js
@@ -13,17 +13,34 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating')
-      .then(potato => potato.json())
-      .then(json => console.log(json))
-      .catch(err => console.log(err))
-
+    this._getMovies();
   }
 
   _renderMovies = () => {
-    return this.state.movies.map((movie, idx) => {
-      return <Movie title={movie.title} poster={movie.poster} key={idx} />;
+    console.log(this.state.movies);
+    return this.state.movies.map(movie => {
+      return (
+        <Movie
+          title={movie.title}
+          poster={movie.large_cover_image}
+          key={movie.id}
+        />
+      );
     });
+  };
+
+  _getMovies = async () => {
+    const movies = await this._callApi();
+    this.setState({
+      movies,
+    });
+  };
+
+  _callApi = () => {
+    return fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating')
+      .then(potato => potato.json())
+      .then(json => json.data.movies)
+      .catch(err => console.log(err));
   };
 
   render() {

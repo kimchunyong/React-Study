48ff49b077f73952a46a05931a0069d0d2b41a83
kimchunyong <lovemy58511@gmail.com>
2019-04-22 23:39:29 +0900

    [add] restAPI 연습

---
+++ restapi-pratice/rest-api/src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  state = {};
+  componentDidMount() {
+    this._getMovies();
+  }
+
+  _getMovies = async () => {
+    const bookInfo = await this._callApi();
+    this.setState({
+      bookInfo,
+    });
+  };
+
+  _callApi = () => {
+    const _apiKey = 'KakaoAK 6c01e7d9aa8f3566e902627b81bfd6a9';
+    const _url = 'https://dapi.kakao.com/v3/search/book';
+    const _getQuery = `?query=미움받을 용기`;
+
+    const _myHeaders = new Headers();
+    _myHeaders.append('Authorization', _apiKey);
+
+    const _init = {
+      method: 'get',
+      headers: _myHeaders,
+    };
+
+    return fetch(_url + _getQuery, _init)
+      .then(res => res.json())
+      .then(json => json.documents)
+      .catch(err => console.log(err));
+  };
+  render() {
+    return <div className="App">1</div>;
+  }
+}
+
+export default App;

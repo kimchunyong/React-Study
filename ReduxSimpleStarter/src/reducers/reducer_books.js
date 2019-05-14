9515e9972a360844b3d2bf5dce9499b5080a9669
kimchunyong <lovemy58511@gmail.com>
2019-05-14 23:02:21 +0900

    [add] redux 연습하기

---
+++ ReduxSimpleStarter/src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+  return [
+    { title: 'javasript The Good' },
+    { title: 'Harry Potter' },
+    { title: 'The Dark Tower' },
+    { title: 'Eloquent Ruby' },
+    { title: 'enjoy life' },
+  ];
+}

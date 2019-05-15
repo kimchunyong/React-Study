7ca263ee1d2ef02312ca31122cc5d96cb07d3442
kimchunyong <lovemy58511@gmail.com>
2019-05-15 19:14:20 +0900

    [fech] redux연습

--- ReduxSimpleStarter/src/containers/book-list.js
+++ ReduxSimpleStarter/src/containers/book-list.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import {selectBook} from '../actions/index';
+import {bindActionCreators} from 'redux';
+
 class BookList extends Component {
   renderList() {
     return this.props.books.map(book => {
       return (
-        <li key={book.title} className="list-group-item">
+        <li 
+          key={book.title} 
+          className="list-group-item"
+          onClick={()=> this.props.selectBook(book)}
+        >
           {book.title}
         </li>
       );
@@ -21,10 +28,16 @@ class BookList extends Component {
   }
 }
 
+//store에 state연결하기
 function mapSateToProps(state) {
   return {
     books: state.books,
   };
 }
 
-export default connect(mapSateToProps)(BookList);
+//액션생성자 연결하기
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({selectBook:selectBook},dispatch);
+}
+
+export default connect(mapSateToProps,mapDispatchToProps)(BookList);

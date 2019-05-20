6d4acb9da496efd93413f0bc7b13ece77f2076e8
kimchunyong <lovemy58511@gmail.com>
2019-05-20 11:53:14 +0900

    [add] redux 연습

--- ReduxSimpleStarter/src/reducers/reducer_books.js
+++ ReduxSimpleStarter/src/reducers/reducer_books.js
@@ -1,9 +1,9 @@
 export default function() {
   return [
-    { title: 'javasript The Good' },
-    { title: 'Harry Potter' },
-    { title: 'The Dark Tower' },
-    { title: 'Eloquent Ruby' },
-    { title: 'enjoy life' },
+    { title: 'javasript The Good', pages:101 },
+    { title: 'Harry Potter' , pages:39},
+    { title: 'The Dark Tower', pages:85},
+    { title: 'Eloquent Ruby',pages:1 },
+    { title: 'enjoy life',pages:5 },
   ];
 }

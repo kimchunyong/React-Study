f0906a5e96d5ee38b5c64339034fff37a23c20c0
kimchunyong <lovemy58511@gmail.com>
2019-07-30 22:45:55 +0900

    [add] list 부분 구현 완료

--- react-firebase/src/components/list/index.js
+++ react-firebase/src/components/list/index.js
@@ -2,6 +2,40 @@ import React, { Component } from 'react';
 
 import { withFirebase } from '../Firebase';
 
+import styled from 'styled-components';
+
+const VideoBox = styled.div`
+    max-width: 720px;
+    width:100%;
+    margin: 0 auto;
+    padding:20px;
+    text-align:center;
+`
+const VideoView = styled.video`
+    display: block;
+    width: 100%;
+`
+
+const VideoTitle = styled.h1`
+    text-align: left;
+    padding: 10px;
+    font-weight: 700;
+    color: #595959;
+    border: 1px solid #9e9e9e;
+    border-top-left-radius: 12px;
+    border-top-right-radius: 12px;
+`
+const VideoContenst = styled.span`
+    display: block;
+    text-align: left;
+    padding: 10px;
+    border: 1px solid #9e9e9e;
+`
+
+const VideoUser = styled(VideoContenst)`
+    border-top:0;
+`
+
 class List extends Component {
     state = {
         data: null,
@@ -23,14 +57,14 @@ class List extends Component {
 
                                 value.fileUrl
                                     ?
-                                    <div key={idx}>
-                                        <h1>{value.inpTitle}</h1>
-                                        {value.fileUrl === '' ? '비디오가 없습니다.' : <video controls ><source src={value.fileUrl} type="video/mp4"></source></video>}
+                                    <VideoBox key={idx}>
+                                        <VideoTitle>{idx + 1}.{value.inpTitle}</VideoTitle>
+                                        {value.fileUrl === '' ? '비디오가 없습니다.' : <VideoView controls ><source src={value.fileUrl} type="video/mp4"></source></VideoView>}
                                         <p>
-                                            <span>{value.contentsTxt}</span>
-                                            <span>{value.userMail}</span>
+                                            <VideoContenst>내용: {value.contentsTxt}</VideoContenst>
+                                            <VideoUser>작성자: {value.userMail}</VideoUser>
                                         </p>
-                                    </div>
+                                    </VideoBox>
                                     :
                                     ""
 
